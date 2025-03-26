d85c99d8f92aa1ded6cf8ba3c0aa1f822fcef491
ycz <yczaubhreydy@gmail.com>
2025-03-26 17:35:25 +0800

    user_input

---
+++ check_if_it_ends_with_without_using_endswith()_function.py
@@ -0,0 +1,4 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# 1. Ask for user input.
+string_input = input("Input any string:  ")
