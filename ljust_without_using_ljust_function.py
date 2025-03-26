0ed1b7ccff0fad539cda36c48399ed3df11daea3
ycz <yczaubhreydy@gmail.com>
2025-03-26 17:51:17 +0800

    assigning_variables

---
+++ ljust_without_using_ljust_function.py
@@ -0,0 +1,7 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# 1. Ask for the user input.
+string_input = input("Enter any string: ")
+
+# 2. Assign length of characters.
+width = 100
