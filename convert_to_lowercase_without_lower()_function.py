88d1049872406ccbd164722da7571a261203ecfe
ycz <yczaubhreydy@gmail.com>
2025-03-26 16:54:21 +0800

    user_input

---
+++ convert_to_lowercase_without_lower()_function.py
@@ -0,0 +1,4 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# 1. Ask the user for string input.
+string_input = input("Input any string: ")
