8e384318ba3545a8fa75470dba0d75df440bd58a
ycz <yczaubhreydy@gmail.com>
2025-03-26 18:05:43 +0800

    user_input

---
+++ swapcasing_withoout_swapcase()_function.py
@@ -0,0 +1,5 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# 1. Ask for user input.
+string_input = input("Input any string: ")
+
