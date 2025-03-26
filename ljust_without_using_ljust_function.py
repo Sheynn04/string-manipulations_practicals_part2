4ce2c92c3a4ac591d16e987d936d4ccbcac24b6a
ycz <yczaubhreydy@gmail.com>
2025-03-26 17:56:38 +0800

    adjust_to_left_without_using_ljust()_function

--- ljust_without_using_ljust_function.py
+++ ljust_without_using_ljust_function.py
@@ -5,3 +5,11 @@ string_input = input("Enter any string: ")
 
 # 2. Assign length of characters.
 width = 100
+
+# 3. Check if the input has the same length of characters as the width. 
+# 4. Add spaces in the end if character length is less than width.
+# 5. Print the ljust string.
+if len(string_input) < width:
+    string_input += " " * (width-len(string_input))
+
+print(string_input)
