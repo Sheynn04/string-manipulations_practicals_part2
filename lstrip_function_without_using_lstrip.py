e34788f613ff3904b9cf943f32382627d70d2496
ycz <yczaubhreydy@gmail.com>
2025-03-26 16:29:24 +0800

    individual_character_accessing

--- lstrip_function_without_using_lstrip.py
+++ lstrip_function_without_using_lstrip.py
@@ -3,4 +3,5 @@
 # 1. Ask  the user to input anything with spaces at the front.
 string_input = input("Input anything with spaces on the front: ")
 
-
+# 2. Create a loop so we can access each character in the input 
+for i, char in enumerate(string_input):
