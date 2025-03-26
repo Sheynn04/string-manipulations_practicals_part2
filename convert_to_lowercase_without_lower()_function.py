7b3b6923a33153c30692f95abfd77fe0fe769ee7
ycz <yczaubhreydy@gmail.com>
2025-03-26 17:07:43 +0800

    convert_upper_to_lower_case

--- convert_to_lowercase_without_lower()_function.py
+++ convert_to_lowercase_without_lower()_function.py
@@ -2,7 +2,15 @@
 
 # 1. Ask the user for string input.
 string_input = input("Input any string: ")
-
+final_string = ""
 # 2. Access the characters in the string.
 for char in string_input:
-    
+
+# 3. Check if any character is uppercase.
+# 4. Convert the uppercase characters to lowercase by adding 32 because that is the difference between upper and  lowercase. 
+    if "A" <= char <= "Z":
+        final_string += chr(ord(char) + 32)
+    else:
+        final_string += char
+# 5. Print the final string.
+print(final_string)
