ae66e0734428ae5ba9f29c276d4d680f049281ca
ycz <yczaubhreydy@gmail.com>
2025-03-26 17:44:57 +0800

    check_if_it_ends_with_suffix_without_endswith()_function

--- check_if_it_ends_with_without_using_endswith()_function.py
+++ check_if_it_ends_with_without_using_endswith()_function.py
@@ -5,3 +5,10 @@ string_input = input("Input any string:  ")
 
 # 2. Assign a suffix.
 ends_with = "dy"
+
+# 3. Check if the input ends with the suffix.
+# 4. Print True if yes, False if not.
+if string_input[-len(ends_with):] == ends_with:
+    print("True")
+else:
+    print("False")
