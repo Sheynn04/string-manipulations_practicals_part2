e7a236b59d3778cb6309e6aa867d82fada547e16
ycz <yczaubhreydy@gmail.com>
2025-03-26 18:00:57 +0800

    assigning_variables

---
+++ center_without_center()_function.py
@@ -0,0 +1,5 @@
+# Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# Ask for user input. 
+string_input = input("Input any string: ")
+width = 100
