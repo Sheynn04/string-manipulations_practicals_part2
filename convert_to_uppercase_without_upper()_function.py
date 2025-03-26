6667c21a6d8b8bffb73f55a06a0f20a9c8839750
ycz <yczaubhreydy@gmail.com>
2025-03-26 17:11:30 +0800

    user_input

---
+++ convert_to_uppercase_without_upper()_function.py
@@ -0,0 +1,4 @@
+# Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# 1. Ask the useer to input.
+string_input = input("Input any string: ")
