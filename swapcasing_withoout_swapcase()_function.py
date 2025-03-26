dcfd14dd7f62fa2e6f8a886a8d39503a31b16e7c
ycz <yczaubhreydy@gmail.com>
2025-03-26 18:06:42 +0800

    swap_casing_of_letters

--- swapcasing_withoout_swapcase()_function.py
+++ swapcasing_withoout_swapcase()_function.py
@@ -3,3 +3,12 @@
 # 1. Ask for user input.
 string_input = input("Input any string: ")
 
+# 2. Reverse the casing of each character.
+swapped_string = ""
+for char in string_input:
+    if "A" <= char <= "Z":  # If uppercase, convert to lowercase
+        swapped_string += chr(ord(char) + 32)
+    elif "a" <= char <= "z":  # If lowercase, convert to uppercase
+        swapped_string += chr(ord(char) - 32)
+    else:
+        swapped_string += char  # Keep non-alphabet characters the same
